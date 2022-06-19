ffa5c4e58315313065ce1cfe56f8f526402ef3ee
Kasper Kain <kasperkain@gmail.com>
2022-06-18 20:21:44 -0400

    clean-up

--- src/components/CustomLink/CustomLink.jsx
+++ src/components/CustomLink/CustomLink.jsx
@@ -2,7 +2,7 @@ import './CustomLink.css'
 import { Link } from 'react-router-dom'
 
 export default function CustomLink({ href, name, active = 'false' }) {
-    return <li>
+    return <li className='CustomLink'>
         <Link className={active} to={href}>{name}</Link>
     </li>
 }
