53a22caa610d71b6d6bd32b35e8abebdd66deea3
Kasper Kain <kasperkain@gmail.com>
2022-06-18 23:21:06 -0400

    created about info

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -3,6 +3,12 @@ import LoadingIcon from '../../components/LoadingIcon/LoadingIcon'
 
 export default function AboutPage() {
     return <div className='AboutPage'>
-
+        <div className='about-info-container'>
+            <h1>ABOUT...</h1>
+            <p>Created in the hopes of impressing a potential employer.
+                Well, current employer, different department, kinda. <br />
+                Whatever
+            </p>
+        </div>
     </div>
 }
